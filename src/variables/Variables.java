d1de2dfbfc1e4bae35c1b44842b7132ffba77258
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 11:16:36 -0400

    Agregados comentarios

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -3,11 +3,18 @@ package variables;
 public class Variables {
 
     public static void main(String[] args) {
+        // variable de tipo int, de nombre telefono, inicializada
         int telefono = 1234567;
+        // variable de tipo char
         char letra = 'G';
+        /* variable de tipo float
+           nombre peso 
+           es necesario hacer un casteo en tipo float 
+           para hacerlo más corto se pone una f al final */
         float peso = 84.3f;
         double ngrande = 76.230339523043430493205;
         
+        // imprimir en consola 
         System.out.println("El número de teléfono es " + telefono);
         System.out.println("La letra es " + letra);
         System.out.println("El peso es " + peso);
